fc805ab27ce353af45df3867a26b9eaa6ae944da
WellenLee <wiiw337@gmail.com>
2020-04-20 16:53:57 +0800

    testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,35 @@
-var numbers = ["a", "b", "c", "d", "e"];
+var numbers = ["A", "B", "C", "D", "E"];
 
 function randomBTN() {
-    var rnr1 = numbers[Math.floor(Math.random() * numbers.length)];
-    document.getElementById("result1").innerHTML = rnr1;
+    var randNum1 = numbers[Math.floor(Math.random() * numbers.length)];
+    document.getElementById("result1").innerHTML = randNum1;
 
-    var rnr2 = numbers[Math.floor(Math.random() * numbers.length)];
-    document.getElementById("result2").innerHTML = rnr2;
+    var randNum2 = numbers[Math.floor(Math.random() * numbers.length)];
+    document.getElementById("result2").innerHTML = randNum2;
 
-    var rnr3 = numbers[Math.floor(Math.random() * numbers.length)];
-    document.getElementById("result3").innerHTML = rnr3;
+    var randNum3 = numbers[Math.floor(Math.random() * numbers.length)];
+    document.getElementById("result3").innerHTML = randNum3;
 
-    var rnr4 = numbers[Math.floor(Math.random() * numbers.length)];
-    document.getElementById("result4").innerHTML = rnr4;
+    var randNum4 = numbers[Math.floor(Math.random() * numbers.length)];
+    document.getElementById("result4").innerHTML = randNum4;
+
+
+    if (randNum1 = numbers) {
+        console.log(true);
+    } else {
+        console.log(false);
+    }
+
+    if (randNum1 = numbers) {
+        $("#pick1").click(function() {
+            console.log(document.getElementById("pick1").innerHTML)
+            console.log(document.getElementById("selected1").innerHTML)
+            document.getElementById("selected1").innerHTML = $("#pick1").innerHTML;
+            console.log(document.getElementById("pick1").innerHTML)
+            console.log(document.getElementById("selected1").innerHTML)
+        })
+
+    }
+
+
 }
